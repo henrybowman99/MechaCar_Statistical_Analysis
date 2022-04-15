80ad45c6591fd7beeaeec124f8feaa573aa2ffd8
Henry Bowman <habowman@wisc.edu>
2022-04-15 10:08:58 -0700

    Add code for Deliverable 3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -11,4 +11,15 @@ suspension_df <- read.csv(file = 'Suspension_Coil.csv', check.names=F, stringsAs
 total_summary <- suspension_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 lot_summary <- suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 
+# t-test for suspension PSI
+t.test(suspension_df$PSI, mu=1500)
 
+#create separate tables for each lot 
+lot1 <- subset(suspension_df, Manufacturing_Lot == 'Lot1')
+lot2 <- subset(suspension_df, Manufacturing_Lot == 'Lot2')
+lot3 <- subset(suspension_df, Manufacturing_Lot == 'Lot3')
+
+#run t test for each lot
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
